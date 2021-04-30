a575f47577bf30c340b5fdea40ed083d97afdfef
JoelMorrisey <joelmorrissey.2408@gmail.com>
2021-04-30 14:58:42 +1000

    added redoc test

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import { RedocStandalone } from 'redoc';
+import spec from './ApiDefinition.json';
+
+function App() {
+  return (
+    <RedocStandalone spec={spec}/>
+  );
+}
+
+export default App;
